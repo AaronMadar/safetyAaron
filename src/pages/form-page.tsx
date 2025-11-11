940fe4ba21f202a8029f7460bcb6b8285f2f997a
Aaron <aaronmadar2655@gmail.com>
2025-11-11 10:18:24 +0200

    styleInObject

--- src/pages/form-page.tsx
+++ src/pages/form-page.tsx
@@ -4,26 +4,29 @@ import UnityField from "@/components/component-field/unity";
 import { Description } from "@mui/icons-material";
 import UnitActivity from "@/components/component-field/unitactivity";
 import Activity from "@/components/component-field/activity";
-import KindOfIncident from "../components/component-field/kind-of-Incident";
+import KindOfIncident from "@/components/component-field/kind-of-Incident";
 import Place from "@/components/component-field/place";
 import Damage from "@/components/component-field/damage";
 import Weather from "@/components/component-field/weather";
-import SeverityIncident from "../components/component-field/severity-incident";
+import SeverityIncident from "@/components/component-field/severity-incident";
 import SendIcon from '@mui/icons-material/Send';
 import DateField from "@/components/component-field/date";
+import { mainBoxCss, innerLeftBoxCss, innerRightBoxCss , buttonSubmitCss } from "@/style/style";
+import { useState } from "react";
 
 
 
 export default function PageForm(){
 
+  const [formData, setFormData] = useState([])
   
 
 return (
     <>
     <Header />
-            <Box sx={{ display: "flex", flexDirection: { xs: "column", sm: "column", md: "row", dl: 'row' }, gap: 2, minHeight: '100vh', paddingTop: 4, backgroundColor: 'background.default', }}>
+            <Box sx={mainBoxCss}>
     
-              <Box sx={{ flex: 1, margin: 2, display: "flex", flexDirection: "column", gap: 2, direction:"rtl"}}>
+              <Box sx={innerLeftBoxCss}>
                 <UnityField />
                 <DateField />
                 <Description />
@@ -31,13 +34,13 @@ return (
                 <Activity />
               </Box>
     
-              <Box sx={{ flex: 1, margin: 2, display: "flex", flexDirection: "column", gap: 2 }}>
+              <Box sx={innerRightBoxCss}>
                 <KindOfIncident />
                 <Place />
                 <SeverityIncident></SeverityIncident>
                 <Damage />
                 <Weather />
-                <Button variant="contained"  endIcon={<SendIcon />} sx={{ width: 'fit-content', alignSelf: { xs: 'center', sm: 'flex-end' }, marginTop: 2 }}>
+                <Button variant="contained"  endIcon={<SendIcon />} sx={buttonSubmitCss}>
                   Send
                 </Button>
               </Box>
